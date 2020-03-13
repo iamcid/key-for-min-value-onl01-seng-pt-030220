43c84b530784684111a51d2747fff636ea7199ae
Christopher Cid <iamcid17@gmail.com>
2020-03-13 04:50:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   smallest_num = nil
   name_hash.each do |name, num|
     if smallest_num == nil num < smallest_num
-      smallest_num
     end
+  smallest_num
   end
 end
